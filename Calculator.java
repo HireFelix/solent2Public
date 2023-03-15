7bd090d3ab77ff366bccb63e6d1078830022aaee
Otieno Felix Ongalo <98937311+HireFelix@users.noreply.github.com>
2023-03-16 02:17:09 +0300

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Calculator {
+   public static void main(String[] args) {
+      double num1;
+      double num2;
+      double ans;
+      char op;
+      Scanner reader = new Scanner(System.in);
+      System.out.print("Enter two numbers: ");
+      num1 = reader.nextDouble();
+      num2 = reader.nextDouble();
